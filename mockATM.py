90ebd59e267e1a305f6e3085d60c17b232f6ae63
bny11638 <yaudev2601@gmail.com>
2021-02-06 14:25:10 -0500

    fixing loop

--- mockATM.py
+++ mockATM.py
@@ -29,6 +29,7 @@ def scanQR():
             cv2.putText(img, data, (int(bbox[0][0][0]), int(bbox[0][0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, (0, 255, 0), 2)
             if data:
+                print(data)
                 return data
         # display the image preview
         cv2.imshow("code detector", img)
